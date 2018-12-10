4756ddf694bfefc1564b26352bac25c12a953e43
AnriKoshi <aakashaev@gmail.com>
2018-12-10 22:45:04 +0300

    create - Point

---
+++ src/game/model/Point.java
@@ -0,0 +1,52 @@
+package game.model;
+
+
+public class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + 20 * x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final Point other = (Point) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+}
